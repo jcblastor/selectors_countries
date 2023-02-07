64a0ae1c0b72bfc33e48e2383ecc353af8e9efe7
J. Carlos Guzmán <jcguzmanblack@gmail.com>
2023-02-06 21:12:54 -0300

    Get borders countries and change alphacode for name country

--- src/app/paises/pages/selector-page.component.ts
+++ src/app/paises/pages/selector-page.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { PaisesService } from '../services/paises.service';
-import { CountrySmall } from '../interfaces/countries.interface';
-import { switchMap, tap } from 'rxjs';
+import { CountrySmall, Country } from '../interfaces/countries.interface';
+import { first, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-selector-page',
@@ -14,12 +14,17 @@ export class SelectorPageComponent implements OnInit {
   miFormulario: FormGroup = this.fb.group({
     region: ['', Validators.required],
     country: ['', Validators.required],
+    borders: ['', Validators.required],
   });
+
+  loading: boolean = false;
 
   // llenar selector region
   regiones: string[] = [];
   //llenar selector paises
   countries: CountrySmall[] = [];
+  //llenar selector fronteras
+  borders: CountrySmall[] = [];
 
   constructor(
     private readonly fb: FormBuilder,
@@ -31,11 +36,27 @@ export class SelectorPageComponent implements OnInit {
     //detectar cambio de region
     this.miFormulario.get('region')?.valueChanges.pipe(
       // reseteamos el valor del campo country cada vez que cambie de region
-      tap( (_) => this.miFormulario.get('country')?.reset('') ),
+      tap( (_) => {
+        this.loading = true
+        this.miFormulario.get('country')?.reset('');
+      }),
       // obtiene la respuesta y devuelve un observable, con la nueva info
       switchMap(region => this.paisesService.getcountriesByRegion(region) )
     ).subscribe( countries => {
       this.countries = countries;
+      this.loading = false
+    });
+
+    this.miFormulario.get('country')?.valueChanges.pipe(
+      tap( (_) => {
+        this.loading = true;
+        this.miFormulario.get('borders')?.reset('');
+      }),
+      switchMap( code => this.paisesService.getCountryByCode(code) ),
+      switchMap( country => this.paisesService.getCountriesByCodes(country?.borders!) )
+    ).subscribe( countries => {
+      this.borders = countries;
+      this.loading = false;
     })
   }
 
--- src/app/paises/services/paises.service.ts
+++ src/app/paises/services/paises.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 
-import { Observable } from "rxjs";
+import { combineLatest, Observable, of } from "rxjs";
 
-import { CountrySmall } from '../interfaces/countries.interface';
+import { CountrySmall, Country } from '../interfaces/countries.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -23,5 +23,30 @@ export class PaisesService {
   getcountriesByRegion(region: string): Observable<CountrySmall[]> {
     const url: string = `${this.baseUrl}/region/${region}?fields=name,cca3`;
     return this.http.get<CountrySmall[]>(url);
+  }
+
+  getCountryByCode(code: string): Observable<Country | null> {
+    if ( !code ) return of(null);
+
+    const url = `https://restcountries.com/v2/alpha/${code}`
+    return this.http.get<Country>(url);
+  }
+
+  getCountryByCodeSmall(code: string): Observable<CountrySmall> {
+    const url: string = `${this.baseUrl}/alpha/${code}?fields=name,cca3`;
+    return this.http.get<CountrySmall>(url);
+  }
+
+  getCountriesByCodes(borders: string[]): Observable<CountrySmall[]> {
+    if( !borders ) return of([]);
+
+    const petitions: Observable<CountrySmall>[] = [];
+
+    borders.forEach(code => {
+      const petition = this.getCountryByCodeSmall(code);
+      petitions.push(petition);
+    })
+
+    return combineLatest(petitions);
   }
 }
